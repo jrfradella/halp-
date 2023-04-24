28d9971b3afd87a25cbf664d43e9236a70b183a5
Tanner-Ray-Martin <tanner.ray.martin.123@gmail.com>
2023-04-24 16:10:50 -0500

    func

--- assignment#3.py
+++ assignment#3.py
@@ -1,20 +1,16 @@
-score = input('enter score between 0.0 and 1.0: ')
+score = input("enter score between 0.0 and 1.0: ")
+
+grades = {"A": 0.9, "B": 0.8, "C": 0.7, "D": 0.6, "F": 0}
+
+
+def check_score(score):
+    for grade_letter, minimum_grade in grades.items():
+        if score >= minimum_grade:
+            return grade_letter
+
 
 try:
-    grade = float(score)
-    
-    if grade >= 0.9:
-        print ('A')
-    elif grade >= 0.8:
-        print ('B')
-    elif grade >= 0.7:
-        print ('C')
-    elif grade >= 0.6:
-        print ('D')
-    elif grade < 0.6:
-        print ('F')
-    
-
-except:
-    print ('Type a number between 0.0 and 1.0')
-    print ('Goodbye')
+    score = float(score)
+    print(f"Score: {score*100}% | Grade: {check_score(score)}")
+except Exception as e:
+    print(f"Got this error: {e}")
